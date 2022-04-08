82c859fae2d9ba61e897bc8589f2316dd7233c29
gjjun <rlawnstjddl12@gmail.com>
2022-04-08 10:30:45 +0900

    Upload Files
    Upload Files

---
+++ Discord/Discord/Commands/Command.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Xml;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Collections.Generic;
+
+
+namespace Discord.Commands
+{
+    // 모듈 클래스의 접근제어자가 public이면서 ModuleBase를 상속해야 모듈이 인식된다.
+    public class BasicModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("핑")]
+        public async Task PingCommand()
+        {
+            await Context.Channel.SendMessageAsync($"Ping : {Context.Client.Latency}ms");
+        }
+        [Command("사용자정보")]
+        public async Task ReturnInfo(IUser TargetUser = null)
+        {
+            var UserInfo = TargetUser ?? Context.Client.CurrentUser;
+            EmbedBuilder Embed = new EmbedBuilder();
+            Embed.AddField("**이름**", UserInfo.Username);
+            Embed.AddField("**태그**", UserInfo.Discriminator);
+            Embed.AddField("**계정 생성일**", UserInfo.CreatedAt);
+            Embed.AddField("**계정 식별자**", UserInfo.Id);
+            Embed.Color = new Color(0, 255, 0);
+            await ReplyAsync("", false, Embed.Build());
+        }
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [Command("밴")]
+        public async Task DoBan(IUser TargetUser = null, string BanReason = "")
+        {
+            if (TargetUser == null)
+            {
+                await ReplyAsync("밴 사용법 : $밴 <밴 대상> <사유(선택)>");
+                return;
+            }
+            await Context.Guild.AddBanAsync(TargetUser, 0, BanReason);
+            EmbedBuilder Embed = new EmbedBuilder();
+            if(BanReason == String.Empty)
+            {
+                Embed.Description = "**" + Context.User.Username + "님이 " + TargetUser.Username + "님을 밴 하였습니다.**";
+            }
+            else
+            {
+                Embed.Description = "**" + Context.User.Username + "님이 " + TargetUser.Username + "님을 " + BanReason + "의 이유로 밴 하였습니다.**";
+            }
+            Embed.Color = new Color(255, 0, 0);
+            await ReplyAsync("", false, Embed.Build());
+        }
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        [Command("추방")]
+        public async Task DoKiack(SocketGuildUser TargetUser = null, string KickReason = "")
+        {
+            if(TargetUser == null)
+            {
+                await ReplyAsync("추방 사용법 : $추방 <추방대상> <사유(선택)>");
+                return;
+            }
+            await TargetUser.KickAsync(KickReason);
+            EmbedBuilder Embed = new EmbedBuilder();
+            if(KickReason == String.Empty)
+            {
+                Embed.Description = "**" + Context.User.Username + "님이 " + TargetUser.Username + "님을 추방 하였습니다.**";
+            }
+            else
+            {
+                Embed.Description = "**" + Context.User.Username + "님이 " + TargetUser.Username + "님을 " + KickReason + "의 이유로 추방하였습니다";
+            }
+            Embed.Color = new Color(255, 0, 0);
+            await ReplyAsync("", false, Embed.Build());
+        }
+        [Command("현재시간")]
+        public async Task NowTime()
+        {
+            string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            await Context.Channel.SendMessageAsync(time);
+        }
+    }
+}
---
+++ Discord/Discord/Program.cs
@@ -0,0 +1,73 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Win32;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Discord
+{
+    class Program
+    {
+
+        CommandService commands;
+        DiscordSocketClient client;
+        static void Main(string[] args)
+        {
+            new Program().BotMain().GetAwaiter().GetResult();   //봇의 진입점 실행
+        }
+        public async Task BotMain()
+        {
+            commands = new CommandService(new CommandServiceConfig()
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+            client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            //로그 수신 시 로그 출력 함수에서 출력되도록 설정
+            client.Log += OnClientLogReceived;
+            commands.Log += OnClientLogReceived;
+
+            await client.LoginAsync(TokenType.Bot, "Token HERE"); //봇의 토큰을 사용해 서버에 로그인
+            await client.StartAsync();                         //봇이 이벤트를 수신하기 시작
+
+            client.MessageReceived += OnClientMessage;         //봇이 메시지를 수신할 때 처리하도록 설정
+            await commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+            await Task.Delay(-1);   //봇이 종료되지 않도록 블로킹
+        }
+
+        private async Task OnClientMessage(SocketMessage arg)
+        {
+            //수신한 메시지가 사용자가 보낸 게 아닐 때 취소
+            var message = arg as SocketUserMessage;
+            if (message == null) return;
+
+            int pos = 0;
+
+            //메시지 앞에 !이 달려있지 않고, 자신이 호출된게 아니거나 다른 봇이 호출했다면 취소
+            if (!(message.HasCharPrefix('$', ref pos) ||
+             message.HasMentionPrefix(client.CurrentUser, ref pos)) ||
+              message.Author.IsBot)
+                return;
+
+            var context = new SocketCommandContext(client, message);                    //수신된 메시지에 대한 컨텍스트 생성   
+
+            var result = await commands.ExecuteAsync(
+                context: context,
+                argPos: pos,
+                services: null);
+
+        }
+
+        private Task OnClientLogReceived(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());  //로그 출력
+            return Task.CompletedTask;
+        }
+    }
+}
